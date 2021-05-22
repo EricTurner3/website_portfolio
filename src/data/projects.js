7c49893c142892ea746f0f6097cc6af752dd4d43
Eric Turner <me@ericturner.it>
2021-05-22 09:42:10 -0400

    added more projects, updated IT header link

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,36 @@
 const data_projects = {
     "en":[
         {
+            title: "FB Messenger Chatbot",
+            types:[
+                {
+                    label: "Web"
+                }
+            ],
+            description: "Although no longer active, I made a chatbot in Node.JS to help us decide where to go to dinner! It was pre-populated with some restaurants based on our interests.",
+            year: "2018",
+            links: [
+            {
+                label: "GitHub",
+                url: "https://github.com/EricTurner3/restaurant-picker-bot",
+            },
+            ],
+        },
+        {
+            title: "Automotive CANBus Interface",
+            types:[
+                {label: "Python"}, {label: "Automotive"}
+            ],
+            description: "An on-going side project of mine is to build a custom entertainment system for my mustang by working with the CANBus!",
+            year: "2019",
+            links: [
+            {
+                label: "GitHub",
+                url: "https://github.com/EricTurner3/python/tree/master/obd",
+            }
+            ],
+        },
+        {
             title: "Prom Ticket Web App",
             types:[
                 {
@@ -17,11 +47,25 @@ const data_projects = {
             ],
         },
         {
-            title: "Random Person Generator",
+            title: "Device Checkin Web App",
             types:[
                 {
-                    label: "Python"
+                    label: "Web"
                 }
+            ],
+            description: "Also at work, I created another PHP MVC website that integrated with our SIS and Asset Manager to facilitate teachers checking in their student's devices at the end of the year.",
+            year: "2019",
+            links: [
+            {
+                label: "Blog Writeup",
+                url: "hhttps://blog.ericturner.it/2019/03/05/end-of-year-device-check-in-app/",
+            },
+            ],
+        },
+        {
+            title: "Random Person Generator",
+            types:[
+                {label: "Python"}
             ],
             description: "A Python Script that generates a plethora of details for a non-existent person",
             year: "2020",
@@ -39,9 +83,7 @@ const data_projects = {
         {
             title: "Mustang: Digital Speedometer Retrofit",
             types:[
-                {
-                    label: "Automotive"
-                }
+                {label: "Automotive"}
             ],
             description: "With some help of the people on Mustang6g.com, I retrofitted a new entertainment system and speedometer from a newer generation car into my 2015 Mustang!",
             year: "2020",
@@ -53,31 +95,23 @@ const data_projects = {
             ],
         },
         {
-            title: "TryHackMe: Kenobi",
+            title: "TryHackMe Writeups",
             types:[
-                {
-                    label: "Cybersecurity"
-                }
+                {label: "Cybersecurity"}
             ],
-            description: "My writeup for the Kenobi room on TryHackMe! This room goes over SMB Discovery, Exploitation, and Privilege Esclation on a Linux machine!",
+            description: "I have completed several rooms on TryHackMe for pentesting and you can find those writeups on my blog",
             year: "2021",
             links: [
             {
-                label: "Blog Writeup",
-                url: "https://blog.ericturner.it/2021/03/22/tryhackme-kenobi/",
-            },
-            {
-                label: "Room",
-                url: "https://tryhackme.com/room/kenobi",
+                label: "Blog Posts",
+                url: "https://blog.ericturner.it/category/tryhackme/",
             }
             ],
         },
         {
             title: "My Portfolio Website: 2021 Revamp",
             types:[
-                {
-                    label: "Web"
-                }
+                {label: "Web"}
             ],
             description: "This website! Built with React, Gatsby JS, Tailwind CSS, and more!",
             year: "2021",
@@ -87,15 +121,76 @@ const data_projects = {
                 url: "https://github.com/EricTurner3/website_portfolio",
             }
             ],
+        },
+        {
+            title: "Police Data Accessibility Project - App",
+            types:[
+                {label: "Web"}, 
+                {label: "Django"}
+            ],
+            description: "One of my volunteer projects, I worked on the initial construction of the PDAP Django app which is used for displaying agencies and status for US Police Agencies!",
+            year: "2021",
+            links: [
+            {
+                label: "PDAP GitHub",
+                url: "https://github.com/Police-Data-Accessibility-Project/PDAP-app",
+            },
+            {
+                label: "My GitHub",
+                url: "https://github.com/EricTurner3/pdap-intake-ui",
+            }
+            ],
+        },
+        {
+            title: "Police Data Accessibility Project - ETL / Scrapers",
+            types:[
+                {label: "Python"}
+            ],
+            description: "For the same org, I also was one of the contributers for some scrapers (CityProtect) and the main contributor for an automated ETL process for loading data into DoltHub!",
+            year: "2021",
+            links: [
+            {
+                label: "GitHub",
+                url: "https://github.com/Police-Data-Accessibility-Project/PDAP-Scrapers",
+            }
+            ],
         }
     ],
     "it":[
         {
-            title: "Applicazione web del biglietto del ballo",
+            title: "FB Messenger Chatbot",
             types:[
                 {
                     label: "Web"
                 }
+            ],
+            description: "Anche se non è più attivo, ho creato un chatbot in Node.JS per aiutarci a decidere dove andare a cena! Era pre-popolato con alcuni ristoranti basati sui nostri interessi.",
+            year: "2018",
+            links: [
+            {
+                label: "GitHub",
+                url: "https://github.com/EricTurner3/restaurant-picker-bot",
+            },
+            ],
+        },
+        {
+            title: "Interfaccia CANBus in macchina",
+            types:[
+                {label: "Python"}, {label: "Automotive"}
+            ],
+            description: "Un mio progetto secondario in corso è quello di costruire un sistema di intrattenimento personalizzato per la mia mustang lavorando con il CANBus!",
+            year: "2019",
+            links: [
+            {
+                label: "GitHub",
+                url: "https://github.com/EricTurner3/python/tree/master/obd",
+            }
+            ],
+        },
+        {
+            title: "Applicazione web del biglietto del ballo",
+            types:[
+                {label: "Web"}
             ],
             description: "Per il mio lavoro, ho creato un'interfaccia web in PHP con Bootstrap per aiutare a vendere i biglietti, fare il check-in all'evento e contare i voti per il re e la regina del ballo!",
             year: "2019",
@@ -107,11 +202,25 @@ const data_projects = {
             ],
         },
         {
-            title: "Generatore di persone casuali",
+            title: "Web app per il check-in dei dispositivi",
             types:[
                 {
-                    label: "Python"
+                    label: "Web"
                 }
+            ],
+            description: "Ho creato un altro sito web PHP MVC che si è integrato con il nostro SIS e Asset Manager per facilitare agli insegnanti il controllo dei dispositivi dei loro studenti alla fine dell'anno.",
+            year: "2019",
+            links: [
+            {
+                label: "Scrivere sul blog (en)",
+                url: "hhttps://blog.ericturner.it/2019/03/05/end-of-year-device-check-in-app/",
+            },
+            ],
+        },
+        {
+            title: "Generatore di persone casuali",
+            types:[
+                {label: "Python"}
             ],
             description: "Uno script Python che genera informazioni su una persona casuale (non esistente).",
             year: "2020",
@@ -129,9 +238,7 @@ const data_projects = {
         {
             title: "Mustang: Retrofit del Tachimetro Digitale",
             types:[
-                {
-                    label: "Automotive"
-                }
+                {label: "Automotive"}
             ],
             description: "Con l'aiuto delle persone su Mustang6g.com, ho montato un nuovo sistema di intrattenimento e un tachimetro da un'auto di nuova generazione nella mia Mustang del 2015!",
             year: "2020",
@@ -143,31 +250,23 @@ const data_projects = {
             ],
         },
         {
-            title: "TryHackMe: Kenobi",
+            title: "TryHackMe",
             types:[
-                {
-                    label: "Cybersecurity"
-                }
+                {label: "Cybersecurity"}
             ],
-            description: "Il mio writeup per la stanza di Kenobi su TryHackMe!",
+            description: "Ho completato diverse stanze su TryHackMe! Potete trovare quegli scritti qui",
             year: "2021",
             links: [
             {
-                label: "Scrivere sul blog (en)",
-                url: "https://blog.ericturner.it/2021/03/22/tryhackme-kenobi/",
-            },
-            {
-                label: "Room",
-                url: "https://tryhackme.com/room/kenobi",
+                label: "Post del Blog",
+                url: "https://blog.ericturner.it/category/tryhackme/",
             }
             ],
         },
         {
             title: "My Portfolio Website: 2021 Revamp",
             types:[
-                {
-                    label: "Web"
-                }
+                {label: "Web"}
             ],
             description: "Questo sito web! Costruito con React, Gatsby JS, Tailwind CSS, e altro ancora!",
             year: "2021",
@@ -175,6 +274,39 @@ const data_projects = {
             {
                 label: "GitHub",
                 url: "https://github.com/EricTurner3/website_portfolio",
+            }
+            ],
+        },
+        {
+            title: "Police Data Accessibility Project - App",
+            types:[
+                {label: "Web"}, 
+                {label: "Django"}
+            ],
+            description: "Uno dei miei progetti di volontariato, ho lavorato alla costruzione iniziale dell'app PDAP Django che viene utilizzata per visualizzare le agenzie e lo stato delle agenzie di polizia degli Stati Uniti!",
+            year: "2021",
+            links: [
+            {
+                label: "PDAP GitHub",
+                url: "https://github.com/Police-Data-Accessibility-Project/PDAP-app",
+            },
+            {
+                label: "My GitHub",
+                url: "https://github.com/EricTurner3/pdap-intake-ui",
+            }
+            ],
+        },
+        {
+            title: "Police Data Accessibility Project - ETL / Scrapers",
+            types:[
+                {label: "Python"}
+            ],
+            description: "Per la stessa organizzazione, sono stato anche uno dei collaboratori per alcuni scrapers (CityProtect) e il principale collaboratore per un processo ETL automatizzato per caricare i dati in DoltHub!",
+            year: "2021",
+            links: [
+            {
+                label: "GitHub",
+                url: "https://github.com/Police-Data-Accessibility-Project/PDAP-Scrapers",
             }
             ],
         }
