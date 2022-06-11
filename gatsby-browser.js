dfb8c8d9a56c7063cd82200c77a543071618a036
Eric Turner <me@ericturner.it>
2022-06-11 19:35:58 -0400

    fix #13

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,27 @@ const $ = require("jquery")
 export const onInitialClientRender = () => {
   $(document).ready(function () {
       console.log('jQuery Loaded');
-      $($('footer')[1]).hide(); // I can't figure out why, but on production build the footer appears twice. Only does in prod and not dev. Just hide it.
+     
+      // 11 Jun 2022 - Weird build issues where elements are duplicating outside of #___gatsby
+      // this will enumerate the elements in the body and remove any duplicates.
+      $.each($('body').children(), function(i){
+        // only elements in body that should exist are div#___gatsby, div#gatsby-announcer and a bunch of scripts
+        var e = $(this)[0];
+        //console.log(e);
+        if (e.id == '___gatsby'){}
+          //console.log(' * Found #___gatsby, skipping...');
+        else if (e.id == 'gatsby-announcer' ){}
+          //console.log(' * Found #gatsby-announcer, skipping...');
+        else if (e.tagName == 'SCRIPT'){}
+          //console.log(' * Found script, skipping...');
+        else{
+          console.log(' * Removing erroneous element: ');
+          console.log(e);
+          e.remove();
+        }
+          
+      });
+
     $(document).on("click", ".filter-category", function(){
         var cat = $(this).data('skill');
         console.log('Projects - Filtering for ' + cat);
